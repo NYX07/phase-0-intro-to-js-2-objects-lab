dd4dacf2661ba2705956ce3c03cf68c451a7d462
Yong Xin <mycodegenesis@gmail.com>
2022-09-15 05:43:47 +0000

    commit tree b1525306c2fe6f9b07f08bba6aad5a52286f5268

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Noble",
+    streetAddress: "Accra Cantoments",
+};
+employee["Sam"] = "undefined";
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    newEmployee[key];
+
+    delete newEmployee[key];
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    return employee[key];
+}
